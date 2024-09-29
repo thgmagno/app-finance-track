790f4b138bc0edafc060c5d774744dbfbae98864
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-09-29 00:33:26 -0300

    change error message color

--- src/components/shared/ErrorMessage.tsx
+++ src/components/shared/ErrorMessage.tsx
@@ -1,6 +1,6 @@
 export function ErrorMessage({ message }: { message?: string | string[] }) {
   return message ? (
-    <div className="text-sm text-muted text-red-600">
+    <div className="text-sm text-muted text-orange-300">
       <p>{message}</p>
     </div>
   ) : null
